c064d47379e5a904b6a15442eccd7bb459d2a17e
Vadim <uvolkov@374@gmail.com>
2022-10-19 21:53:06 +0700

    Add task1_sem3

--- Seminar#2/task4.py
+++ Seminar#2/task4.py
@@ -3,26 +3,6 @@
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
-# n = int(input("Введите значение n: "))
-# lst = []
-# index = 0
-# while n != -n:
-#    lst[index] = n
-#    index += 1
-#    n -= 1
-# print(n)
-  
-# with open('Seminar#2/test03.txt', 'r') as my_file:
-#    my_file.readline()
-#    data = my_file.read()
-# lst = data.split()
-# print(lst)
-# int(lst[0])
-
-# N = int(input("Введите размер списка: "))
-# spam = list(range(1, N+1))
-# print(spam)
-
 with open('Seminar#2/test03.txt', 'r') as my_file:
    data = my_file.read()
 index = 0
@@ -33,5 +13,5 @@ for i in range(-n, n + 1):
 print(lst)
 
 while index < len(lst):
-   lst[index] *= 5
+   lst[index] *= int(data)
 print(lst)
